2b4a1fbbfdc719b79df9c4d79fc3e19618a8abab
Ayush19bansal <bansalaashu7@gmail.com>
2023-12-21 21:46:24 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './Component/Card';
+
+function App() {
+  return (
+    <div class="container">
+   <Card image="https://downloadhub4u.top/wp-content/uploads/2023/12/The-Freelancer-Season-1-2023-Hindi-Web-Series-Complete-All-Episodes-WEBRip-ESubs-1080p-720p-480p-Download-1.jpg" h2="THE FREELANCER" year="2023," cast=" Mohit Raina" time="117 min" thrill="Action, Adventure, Thriller" paragraph="A single misplaced word could mean death, and a new bride finds herself in such a situation in a conflict-ridden land. Who will save her?" sideImg="https://m.media-amazon.com/images/M/MV5BMWY3YWYyNDQtZTAwNC00M2U3LWI1YzMtZWMxZDFlZTFmY2Y5XkEyXkFqcGdeQXVyMTE0MzQwMjgz._V1_QL75_UX500_CR0,0,500,281_.jpg"/>
+
+   <Card image="https://moviezguru.top/wp-content/uploads/2023/11/Animal-2023-Hindi-Full-Movie-1080p-720p-480p-Download-1.jpg" h2="ANIMAL" year="2023," cast=" Ranbir Kapoor" time="120 min" thrill="Action, Crime, Drama" paragraph="A son undergoes a remarkable transformation as the bond with his father begins to fracture, and he becomes consumed by a quest for vengeance." sideImg="https://www.bollymoviereviewz.com/wp-content/uploads/2023/12/Animal-Movie-Review-2-1.jpg"/>
+
+   <Card image="https://cdn.123telugu.com/content/wp-content/uploads/2023/12/Dunki-2.jpg" h2="DUNKI"
+   year="2023," cast=" Shah Rukh Khan" time="160 min" thrill="Action,Comedy, Drama" 
+   paragraph="Four friends from a village in Punjab share a common dream: to go to England. Their problem is that they have neither the visa nor the ticket. A soldier promises to take them to the land of their dreams." sideImg="https://images.indianexpress.com/2023/12/dunki-live-21122023.jpg?w=414"/>
+   
+   </div>
+  );
+}
+
+export default App;
